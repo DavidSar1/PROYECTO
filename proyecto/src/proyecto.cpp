33f75fac4d861b2f6038b28dc10c9f93038f491e
DavidSar1 <135771912+DavidSar1@users.noreply.github.com>
2023-06-19 16:58:56 -0500

    Update proyecto.cpp

--- proyecto/src/proyecto.cpp
+++ proyecto/src/proyecto.cpp
@@ -27,7 +27,8 @@ public:
 		cout<<"1-"<<matrizPreguntas[num][1]<<endl;
 		cout<<"2-"<<matrizPreguntas[num][2]<<endl;
 		cout<<"3-"<<matrizPreguntas[num][3]<<endl;
-		cout<<"4-"<<matrizPreguntas[num][4]<<endl;};
+		cout<<"4-"<<matrizPreguntas[num][4]<<endl;
+	};
 
 };
 
@@ -119,70 +120,6 @@ class clase_calculos{
 
 
 
-class clase_perfilJugador{
-public:	
-	float atr_saldo=0;
-
-	float met_ingresarsaldo(){
-		//con este metodo le pedimos al usuario el dinero que desea ingresar al juego
-		cout<<"Cuanto dinero desea ingresar?";
-		cin<<atr_saldo;
-		return atr_saldo;
-	}
-};
-
-class clase_calculos{
-
-	float met_calcularPorcentajeporPregunta(float saldo){
-		//aqui se calcula el saldo obtenido por cada respuesta y se envia a una variable
-		float saldo2 = (saldo*10)/100;
-		return saldo2;
-	}
-
-	float met_calcularPorcentajeRetirarse(float saldo, int lvl){
-		//aqui se calcula el saldo obtenido por retirarse del juego y se envia a una variable
-		float saldo2 = 0;
-		if (lvl==3){
-			float saldo2 = (saldo*25)/100;
-		}
-		if (lvl==6){
-			float saldo2 = (saldo*50)/100;
-		}
-		if(lvl==9){
-			float saldo2 = (saldo*75)/100;
-		}
-		return saldo2;
-
-	}
-};
-
-
-class clase_menus{
-public:
-	void met_menuInicial(){
-		cout<<"----------Think And Win-------------"<<endl;
-		cout<<"		1-Comenzar Juego"<<endl;
-		cout<<"		2-Recargar saldo"<<endl;
-		cout<<"		4-Retirar saldo"<<endl;
-		cout<<"		0-Cerrar seccion"<<endl;
-		cout<<"------------------------------------"<<endl;
-	}
-	void met_menuIngreso(){
-		cout<<"---------Opciones de inicio---------"<<endl;
-		cout<<"		1-Iniciar seccion"<<endl;
-		cout<<"		2-Registrar nuevo jugador"<<endl;
-		cout<<"		0-Cerrar el juego"<<endl;
-		cout<<"------------------------------------"<<endl;
-	}
-	void met_menuPreguntas(string pregunta,string res1, string res2,string res3,string res4, int nlv){
-		cout<<"---------Nivel ("<<nlv<<"----------"<<endl;
-		cout<<"¿¿"<<pregunta<<"??"<<endl;
-		cout<<"1-"<<res1<<endl;
-		cout<<"2-"<<res2<<endl;
-		cout<<"3-"<<res3<<endl;
-		cout<<"4-"<<res4<<endl;
-	}
-};
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
